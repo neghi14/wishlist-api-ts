688cbbee2494f920ae96b7f174b9eaffcfccf5f2
neghi14 <sambasseynengimote@gmail.com>
2023-06-02 03:23:29 +0100

    feat: created express server

--- config/default.js
+++
@@ -1,8 +0,0 @@
-export default {
-  database: {
-    url: "mongodb://127.0.0.1:27017/wishlist-dev",
-  },
-  application: {
-    port: 1337,
-  },
-};
--- config/production.js
+++
@@ -1,5 +0,0 @@
-export default {
-  database: {
-    url: MONGO_URI,
-  },
-};
