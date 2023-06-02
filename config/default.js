5878bebdc32c589b24bf8de630a45d389f020135
neghi14 <sambasseynengimote@gmail.com>
2023-06-02 01:39:37 +0100

    build: add dependency

---
+++ config/default.js
@@ -0,0 +1,8 @@
+export default {
+  database: {
+    url: "mongodb://127.0.0.1:27017/wishlist-dev",
+  },
+  application: {
+    port: 1337,
+  },
+};
---
+++ config/production.js
@@ -0,0 +1,5 @@
+export default {
+  database: {
+    url: MONGO_URI,
+  },
+};
